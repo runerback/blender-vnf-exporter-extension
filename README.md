9b17e13a9d9d0723b1af162577fffda4e93514cf
runerback <runerback@hotmail.com>
2025-10-23 16:44:19 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blender-vnf-exporter-extension
-Export blender mesh vnf to openscad
+
+Export blender mesh to [OpenSCAD](https://openscad.org/) [VNF](https://github.com/BelfrySCAD/BOSL2/wiki/Tutorial-VNF) *(module with [BOSL2](https://github.com/BelfrySCAD/BOSL2))*
---
+++ __init__.py
@@ -0,0 +1,122 @@
+bl_info = {
+    "name": "Export VNF",
+    "blender": (2, 80, 0),
+    "category": "Mesh",
+}
+
+import os
+from bpy_extras.io_utils import ExportHelper
+from bpy.types import Operator, VIEW3D_MT_edit_mesh
+from bpy.props import StringProperty
+from bpy.utils import register_class, unregister_class
+import bmesh
+
+
+class MeshVNFExporter(Operator, ExportHelper):
+    """VNF Export Script"""
+
+    bl_idname = "mesh.export_vnf"
+    bl_label = "Export VNF"
+    bl_options = {"PRESET"}
+
+    filename_ext = ".scad"
+    filter_glob: StringProperty(default="*.scad", options={"HIDDEN"})
+
+    def execute(self, context):
+        if context.mode != "EDIT_MESH":
+            self.report({"WARNING"}, "enter edit mode")
+            return {"CANCELLED"}
+        # end if
+        mesh = context.edit_object
+        if not mesh or not mesh.data:
+            self.report({"WARNING"}, "select one mesh")
+            return {"CANCELLED"}
+        # end if
+
+        origin_bm = bmesh.from_edit_mesh(mesh.data)
+        origin_tris_idx = {
+            face.index: face for face in origin_bm.faces if len(face.verts) == 3
+        }
+
+        exporting_bm = origin_bm.copy()
+        exporting_bm.verts.ensure_lookup_table()
+        exporting_bm.edges.ensure_lookup_table()
+        exporting_bm.faces.ensure_lookup_table()
+
+        tri_meshs = bmesh.ops.triangulate(
+            exporting_bm,
+            faces=exporting_bm.faces,
+            quad_method="BEAUTY",
+            ngon_method="BEAUTY",
+        )
+        face_idx_map = {}
+        for k, v in tri_meshs["face_map"].items():
+            face_idx_map[v] = face_idx_map.get(v, []) + [k]
+        # end for
+
+        face_map = {
+            key.index: value for key, value in face_idx_map.items()
+        } | origin_tris_idx
+        faces = {}
+        for fdx, f in face_map.items():
+            if isinstance(f, list):
+                for fi in f:
+                    faces[fi.index] = fi.verts
+                # end for
+            else:
+                faces[fdx] = f.verts
+            # end if
+        # end for
+        faceIdxes = list(faces.keys())
+        faceIdxes.sort()
+        vdxess = [[v.index for v in faces.get(idx)] for idx in faceIdxes]
+
+        module_name, _ = os.path.splitext(os.path.basename(self.filepath))
+        with open(self.filepath, "w", encoding="utf-8") as scad:
+            module_lines = [
+                "include <BOSL2/std.scad>",
+                "module {}() {{".format(module_name),
+                "    verts = [",
+            ]
+            for v in exporting_bm.verts:
+                x, y, z = tuple(v.co)
+                module_lines.append(f"        [{x},{y},{z}],")
+            # end for
+            module_lines.extend(["    ];", "    faces = ["])
+            for vdxes in vdxess:
+                module_lines.append(f"        {vdxes},")
+            # end for
+            module_lines.extend(["    ];", "    polyhedron(verts, faces);", "}"])
+            scad.write("\n".join(module_lines))
+        # end with
+
+        self.report({"INFO"}, "exported to: {}".format(self.filepath))
+        return {"FINISHED"}
+
+    # end def
+
+
+# end class
+
+
+def menu_func(self, context):
+    self.layout.operator(MeshVNFExporter.bl_idname)
+
+
+# end def
+
+
+def register():
+    register_class(MeshVNFExporter)
+    VIEW3D_MT_edit_mesh.append(menu_func)
+
+
+# end def
+
+
+def unregister():
+    VIEW3D_MT_edit_mesh.remove(menu_func)
+    unregister_class(MeshVNFExporter)
+
+
+# end def
